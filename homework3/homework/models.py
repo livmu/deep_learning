da0f71da94675727204c6690bd5dadad97a1cd83
livmu <livmu@live.com>
2025-07-28 17:39:00 -0500

    'models'

--- homework3/homework/models.py
+++ homework3/homework/models.py
@@ -71,7 +71,7 @@ class Classifier(nn.Module):
         z = self.batch3(self.conv3(z))
         z = self.max_pool(self.relu(z))
 
-        z = self.dropout(self.avg_pool(z)))
+        z = self.dropout(self.avg_pool(z))
         
         logits = self.conv4(z.squeeze(-1).squeeze(-1)
         return logits
