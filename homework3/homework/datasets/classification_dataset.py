7d229616d67489e2814a8f08f48c7dcfd8a05733
livmu <livmu@live.com>
2025-07-28 18:46:22 -0500

    'models'

--- homework3/homework/datasets/classification_dataset.py
+++ homework3/homework/datasets/classification_dataset.py
@@ -40,11 +40,11 @@ class SuperTuxDataset(Dataset):
             xform = transforms.Compose(
                 [
                     # TODO: fix
-                    transforms.RandomResizedCrop(224, scale=(0.8, 1.0), ratio=(0.9, 1.1)),
+                    #transforms.RandomResizedCrop(224, scale=(0.8, 1.0), ratio=(0.9, 1.1)),
                     transforms.RandomHorizontalFlip(),
-                    transforms.RandomRotation(15),
+                    #transforms.RandomRotation(15),
                     # transforms.ColorJitter(0.9, 0.9, 0.9, 0.1),
-                    transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.2, hue=0.05),
+                    #transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.2, hue=0.05),
                     transforms.ToTensor(),
                 ]
             )
