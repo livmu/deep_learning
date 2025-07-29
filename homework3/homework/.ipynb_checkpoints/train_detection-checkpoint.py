30ecac339c8cd94453eb0b3dd38c4f80cb44acec
livmu <livmu@live.com>
2025-07-28 20:46:37 -0500

    'train'

--- homework3/homework/.ipynb_checkpoints/train_detection-checkpoint.py
+++ homework3/homework/.ipynb_checkpoints/train_detection-checkpoint.py
@@ -45,7 +45,14 @@ def train(
     model.train()
     
     # change
-    train_data = load_data("drive_data/train", transform_pipeline='aug', shuffle=True, batch_size=batch_size, num_workers=2)
+    train_data = load_data(
+        dataset_path="drive_data/train",
+        transform_pipeline="default",
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=2,
+    )
+    train_data = load_data("drive_data/train", transform_pipeline="aug", shuffle=True, batch_size=batch_size, num_workers=2)
     val_data = load_data("drive_data/val", shuffle=False)
     #train_data, val_data = load_data(dataset_path='')
 
