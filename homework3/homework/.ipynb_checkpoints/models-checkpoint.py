9634e942349a49a197dedbb81d36273e304c797e
livmu <livmu@live.com>
2025-07-28 22:38:53 -0500

    'models'

--- homework3/homework/.ipynb_checkpoints/models-checkpoint.py
+++ homework3/homework/.ipynb_checkpoints/models-checkpoint.py
@@ -116,25 +116,25 @@ class Detector(torch.nn.Module):
         self.d1 = nn.Sequential(
             torch.nn.Conv2d(in_channels, layer1, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(layer1),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
         )
         
         self.d2 = nn.Sequential(
             torch.nn.Conv2d(layer1, layer2, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(layer2),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
         )
 
         self.u2 = nn.Sequential(
             nn.Conv2d(layer2, layer1, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(layer1),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
         )
 
         self.u1 = nn.Sequential(
             nn.Conv2d(layer1, layer1, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(layer1),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
         )
 
         self.track_head = nn.Conv2d(layer1, num_classes, kernel_size=1)
