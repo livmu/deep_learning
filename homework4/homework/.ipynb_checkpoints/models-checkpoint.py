3980550adc3d8f0de0f4e76f9b6eacbc8bf2b4a0
livmu <livmu@live.com>
2025-08-11 01:50:13 -0500

    'models'

--- homework4/homework/.ipynb_checkpoints/models-checkpoint.py
+++ homework4/homework/.ipynb_checkpoints/models-checkpoint.py
@@ -144,7 +144,7 @@ class CNNPlanner(torch.nn.Module):
     def __init__(
         self,
         n_waypoints: int = 3,
-        h: int = 32,
+        h: int = 16,
     ):
         super().__init__()
 
@@ -166,7 +166,7 @@ class CNNPlanner(torch.nn.Module):
             nn.BatchNorm2d(h),
         )
 
-        self.conv2 = nn.Conv2d(h, h*2, kernel_size=3, stride=1, padding=1, dilation=2)
+        self.conv2 = nn.Conv2d(16, h*2, kernel_size=3, stride=1, padding=1, dilation=2)
         self.batch2 = nn.BatchNorm2d(h*2)
 
         self.net2 = nn.Sequential(
