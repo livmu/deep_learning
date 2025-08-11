c695a6286ffdf38639d079c0d4001f2c7417585c
livmu <livmu@live.com>
2025-08-11 02:16:01 -0500

    'models'

--- homework4/homework/models.py
+++ homework4/homework/models.py
@@ -168,7 +168,7 @@ class CNNPlanner(torch.nn.Module):
         self.pool = nn.Sequential(
             nn.Dropout(0.1),
             nn.AdaptiveAvgPool2d((1,1)),
-            nn.Linear(h*2, n_waypoints*2),
+            nn.Linear(h*4, n_waypoints*2),
         )
 
     def forward(self, image: torch.Tensor, **kwargs) -> torch.Tensor:
