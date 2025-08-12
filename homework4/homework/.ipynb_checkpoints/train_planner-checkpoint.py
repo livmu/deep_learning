f3937b0866c656426deaebc31986c34881333a9a
livmu <livmu@live.com>
2025-08-11 19:05:38 -0500

    'models'

--- homework4/homework/.ipynb_checkpoints/train_planner-checkpoint.py
+++ homework4/homework/.ipynb_checkpoints/train_planner-checkpoint.py
@@ -97,7 +97,7 @@ def train(
 
     # create loss function and optimizer
     # weights
-    optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=1e-4)
     #weights = torch.tensor([0.2, 0.8, 1.0], device=device)
     if model_name == "mlp_planner": criterion = nn.MSELoss()
     else: criterion = F.smooth_l1_loss
